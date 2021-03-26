09b01f17923436a3a7077dcb0df11e6ae20e7ead
danyanyam <dvbuchko@edu.hse.ru>
2021-03-26 23:04:50 +0300

    dsad

--- main.py
+++ main.py
@@ -8,5 +8,11 @@ app = Flask(__name__)
 def hello_world():
     return render_template('index.html')
 
+
+@app.route('/get/<food>')
+def get_info_about(food):
+    return food
+
+
 if __name__ == "__main__":
-    app.run(host='0.0.0.0', port='8000', debug=True)
+    app.run(host='0.0.0.0', port='5000', debug=True)

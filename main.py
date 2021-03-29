0da95ab5e892546dc3e931f240624de632fad275
danyanyam <dvbuchko@edu.hse.ru>
2021-03-29 14:09:40 +0300

    поисковик по центру экрана с предложениями по запросу

--- main.py
+++ main.py
@@ -1,14 +1,19 @@
 from flask import render_template, request, jsonify, make_response
 from flask import Flask
 
+import re
+
 app = Flask(__name__)
 
 foods = {
     'apple': {'protein': '3g', 'sugar': '10g', 'img': '&#x1f34f;'},
     'orange': {'protein': '2g', 'img': '🍊'},
     'banana': {'protein': '15g', 'img': '&#x1f34c;'},
-    'coconut': {'protein': '30g', 'img': '&#x1F965'}
+    'coconut': {'protein': '30g', 'img': '&#x1F965'},
+    'barbaris': {'protein': '15g', 'img': '&#x1f34c;'}
 }
+
+food_available = list(foods.keys())
 
 @app.route('/')
 def hello_world():
@@ -20,6 +25,23 @@ def get_info_about():
     food_to_search = request.get_json()['text']
     result = foods.get(food_to_search.lower().strip())
     return make_response(jsonify(result), 200)
+
+
+@app.route('/get_known_food', methods=['POST'])
+def get_known_food():
+    food_to_search = request.get_json()['text']
+
+    if food_to_search:
+        answer = []
+        for food in food_available:
+            if re.match(food_to_search, food.strip()):
+                answer.append(food)
+    
+        # jsonify(result)
+        return make_response(jsonify({'result': answer}), 200)
+    return make_response(jsonify({'result': ''}), 200)
+
+
 
 
 if __name__ == "__main__":
--- static/js/post-input.js
+++ static/js/post-input.js
@@ -1,12 +1,58 @@
-const postbtn = document.getElementById('search-btn');
 const input_field = document.getElementById('food-to-search');
 
-const postData = () => {
-    const text = document.getElementById("food-to-search").value;
-    fetch('/post', {
+
+// const postData = () => {
+//     const text = document.getElementById("food-to-search").value;
+//     fetch('/post', {
+//         method: "POST",
+//         credentials: "include",
+//         body: JSON.stringify({text: text}),
+//         cache: "no-cache",
+//         headers: new Headers({
+//             "content-type": "application/json"
+//         })
+//     }).then((response) => {
+//         if (response.status !== 200) {
+//             console.log('NOT FOUND FOOD');
+//             return;
+//         }
+//         response.json().then((data) => {            
+//             appendReceivedData(data);
+//         })
+//     })
+// };
+
+// function appendReceivedData(data) {
+//     var element = document.getElementById("column");
+//     var div = document.createElement('div');
+//     const keys = Object.keys(data);
+    
+//     div.className = "search-box"
+
+//     for (let index = 0; index < keys.length; index++) {
+//         var div_ = document.createElement('div');
+//         if (keys[index] !== 'img') {
+//             div_.innerHTML += `${keys[index]}: `;
+//         }
+//         div_.innerHTML += data[keys[index]];
+//         div.appendChild(div_);   
+//     }
+//     element.appendChild(div);
+// }
+
+// input_field.addEventListener("keyup", function(event) {
+//     event.preventDefault();
+//     if (event.keyCode === 13) {
+//       postData();
+//     }
+//   });
+
+const inputHandler = function(e) {
+    to_search = e.target.value;
+    fetch('/get_known_food', {
         method: "POST",
         credentials: "include",
-        body: JSON.stringify({text: text}),
+        body: JSON.stringify({text: to_search}),
         cache: "no-cache",
         headers: new Headers({
             "content-type": "application/json"
@@ -16,34 +62,40 @@ const postData = () => {
             console.log('NOT FOUND FOOD');
             return;
         }
-        response.json().then((data) => {            
-            appendReceivedData(data);
+        response.json().then((data) => {
+            console.log(data['result']);
+            if (data['result'] !== FOUND) {   
+                prev_search = document.querySelector('[removable]');
+                while (prev_search) {
+                    prev_search.remove()
+                    prev_search = document.querySelector('[removable]');
+                }            
+                handleResponse(data['result']);
+                FOUND = data['result'];
+            }
         })
     })
+    
 };
 
-function appendReceivedData(data) {
-    var element = document.getElementById("column");
-    var div = document.createElement('div');
-    const keys = Object.keys(data);
-    
-    div.className = "search-box"
+var FOUND = false;
 
-    for (let index = 0; index < keys.length; index++) {
-        var div_ = document.createElement('div');
-        if (keys[index] !== 'img') {
-            div_.innerHTML += `${keys[index]}: `;
-        }
-        div_.innerHTML += data[keys[index]];
-        div.appendChild(div_);   
+function handleResponse(response) {
+    var element_ = document.getElementById("column");
+
+    if (response) {
+        var i = 2;
+        response.forEach(element => {
+            var div = document.createElement('div');
+            div.className = 'found';
+            div.setAttribute('removable', 'true');
+            div.setAttribute('tabindex', i);
+            div.innerHTML += element;
+            element_.appendChild(div);
+            i ++;
+        });
     }
-    element.appendChild(div);
 }
 
-input_field.addEventListener("keyup", function(event) {
-    event.preventDefault();
-    if (event.keyCode === 13) {
-      postData();
-    }
-  });
-postbtn.addEventListener('click', postData);
+input_field.addEventListener('input', inputHandler);
+input_field.addEventListener('propertychange', inputHandler);
